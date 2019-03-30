f8d7848d10df1b1caf37fa8a7f0ba2e80a03ffff
thomasjagielski <tjagielski@olin.edu>
2019-03-29 23:34:29 -0400

    Add images into matlab as grayscale copies

--- eigen_resistance.m
+++ eigen_resistance.m
@@ -3,5 +3,23 @@
 %@authors: Thomas Jagielski and Sparsh Bansal
 
 %% Load images
+train = zeros(250,600,3,78);
+for k=0:78
+    train(:,:,:,k+1) = imread(strcat('./initialized_train/', int2str(k), '.png'));
+end
 
-imread('0.png')
+test = zeros(250,600,3,100);
+for k=0:99
+    test(:,:,:,k+1) = imread(strcat('./initialized_test/', int2str(k), '.png'));
+end
+
+%% Convert images to black and white
+bw_train = zeros(250,600,size(train, 4));
+for l=size(train,4)
+    bw_train(:,:,l) = rgb2gray(train(:,:,:,l));
+end
+
+bw_test = zeros(250,600,size(test, 4));
+for l=size(test, 4)
+    bw_train(:,:,l) = rgb2gray(train(:,:,:,l));
+end
